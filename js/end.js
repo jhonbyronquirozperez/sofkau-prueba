25ec24d33a797fdd6db4b77cc27adc38b5756692
jhonbyronquirozperez <jhonbyronquirozperez@gmail.com>
2022-03-24 14:43:02 -0500

    se agrega el guardado de los puntajes  en la localstorage

--- js/end.js
+++ js/end.js
@@ -2,6 +2,12 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+//para almacenar los puntajes en la memoria
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -10,4 +16,18 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
     e.preventDefault();
+};
+
+//el objeto permite guardar los puntajes en localStorage
+
+const score = {
+    score: mostRecentScore,
+    name: username.value,
+};
+highScores.push(score);
+highScores.sort((a, b) => b.score - a.score);
+highScores.splice(5);
+
+localStorage.setItem('highScores', JSON.stringify(highScores));
+window.location.assign('/');
 };
